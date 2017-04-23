38a3987c147d17952929b76a2c1c2d9ff35402de
Calvin Liu <craze@cs.utexas.edu>
2017-04-23 14:33:42 -0500

    created Rapper, added functionality

--- src/test.scala
+++ src/test.scala
@@ -1,9 +1,33 @@
-object test extends Rapper{
+object test extends RapBattle{
+
+	object DJKHALED extends Rapper{
+		name = "DJ Khaled";
+	}
+
+	object SNOOPDOG extends Rapper{
+		name = "Snoop Dog";
+	}
 	def main(args: Array[String]): Unit = {
+		SNOOPDOG YO
+
+		I GOT 5 STACKS
+
 		I GOT THE KEYS KEYS KEYS
+
+		SPIT STRING "Should be 135"
+
+		SPIT FIRE
+
+		DJKHALED YO
+
+		I GOT 6 STACKS
+
+		HALF A MIL
 
 		ANOTHER ONE ONE ONE
 
-		RUN
+		SPIT STRING "Should be 6"
+
+		SPIT FIRE
 	}
 }
